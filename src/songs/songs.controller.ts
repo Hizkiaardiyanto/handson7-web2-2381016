f5f30d8cfb7ce2dc3f2033a8f9b3974fc456b0b5
Hizkiaardiyanto <157075416+Hizkiaardiyanto@users.noreply.github.com>
2025-03-21 11:46:40 +0700

    Update comm

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseInterceptors, ValidationPipe } from '@nestjs/common';
+import { CreateSongDTO } from './create-song.dto';
+import { SongsService } from './songs.service';
+import { ExecutionTime } from 'src/ExecutionTime.interceptor';
 
 @Controller('songs')
-export class SongsController {}
+export class SongsController {
+    constructor(private songService: SongsService) {}
+
+    @Post()
+    @UseInterceptors(ExecutionTime)
+    create(@Body(new ValidationPipe()) createSongDTO: CreateSongDTO) {
+        return this.songService.create(createSongDTO);
+    }
+
+    @Get()
+    findAll() {
+        return this.songService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: number) {
+        return this.songService.findOne(id);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: number, @Body() createSongDTO: CreateSongDTO) {
+        return this.songService.updateOne(id, createSongDTO)
+    }
+
+    @Delete(':id') 
+    delete(@Param('id') id: number) {
+        return this.songService.delete(id)
+    }
+}
